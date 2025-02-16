58f69d53ac8f4b032a449b5c5ea63bc6a8d1eaa6
ub-victor <victoireushindi371@gmail.com>
2025-02-16 21:25:07 +0200

    initial commit on main

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-        // Toggle mobile menu visibility
+        // Toggle mobile menu visibility and adjust main content padding
         document.getElementById('mobile-menu-button').addEventListener('click', function() {
-            var mobileMenu = document.getElementById('mobile-menu');
+            const mobileMenu = document.getElementById('mobile-menu');
+            const mainContent = document.getElementById('main-content')
+
+            // Toggle mobile menu visibility
             mobileMenu.classList.toggle('hidden');
+
+           // Adjust main content padding
+           if (mobileMenu.classList.contains('hidden')){
+            mainContent.classList.remove('pt-48'); // Reset padding if menu is closed
+           }else {
+            mainContent.classList.add('pt-48');  // Add padding if menu is open
+            } 
         });
     
